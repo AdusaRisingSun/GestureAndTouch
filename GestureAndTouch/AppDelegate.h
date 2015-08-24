e8af45a4516c8a65ed3ecab13f622b9724dc5758
AdusaRisingSun <Adusa@adusademac-mini.local>
2015-08-24 10:00:07 +0800

    Initial Commit

---
+++ GestureAndTouch/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GestureAndTouch
+//
+//  Created by Adusa on 15/8/24.
+//  Copyright © 2015年 Adusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ GestureAndTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureAndTouch
+//
+//  Created by Adusa on 15/8/24.
+//  Copyright © 2015年 Adusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
